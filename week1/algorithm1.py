e32981105575bf75f9ef38c2547b9e522e049b63
februarysea <februarysea@februaryseadeMacBook-Pro.local>
2021-03-29 20:16:14 +0800

    getdata

---
+++ week1/algorithm1.py
@@ -0,0 +1,15 @@
+import matplotlib
+import re
+
+def get_distance():
+    pass
+
+def get_price():
+    pass
+
+
+def gradient_descent(x:list, y:list):
+    pass
+
+if __name__ == "__main__":
+    pass
--- week1/gradientdescent.py
+++
@@ -1,8 +0,0 @@
-import matplotlib
-import re
-
-def gradient_descent():
-    pass
-
-if __name__ == "__main__:
-    pass
